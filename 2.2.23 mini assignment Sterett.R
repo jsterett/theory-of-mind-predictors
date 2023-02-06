578050296c662e3b9ccab8afd446630c4829b531
Jordi Sterett <121898392+jsterett@users.noreply.github.com>
2023-02-06 11:16:05 -0600

    Add files via upload
    mini assignment for Feb 2

---
+++ 2.2.23 mini assignment Sterett.R
@@ -0,0 +1,23 @@
+# loading tidyverse
+library(tidyverse)
+# assigning function reading in data
+mmdata <- read_csv("MM Data.csv")
+# changing data to long form
+mmdata.long <- read_csv("MM Data.csv") %>% 
+  pivot_longer(cols = c("Red", "Green", "Blue", "Orange","Yellow", "Brown"),
+      names_to = "Color", values_to = "Number")
+# assigning function to object Color
+Color <- mmdata.long$Color
+# creating first box plot and filling colors
+ggplot(mmdata.long, aes(x=`Color`, y=`Number`, fill = `Color`)) + geom_boxplot()
+# creating second box plot and adding dot points
+ggplot(mmdata.long, aes(x=`Color`, y=`Number`, fill = `Color`)) + geom_boxplot() + geom_jitter()
+
+## mini assignment 2/2/23 
+# first plot recreation
+ggplot(mmdata.long, aes(x=`Color`, y=`Number`, color=`Color`))+
+  geom_violin(alpha=0.0)+geom_jitter() + 
+  scale_color_manual(values = c("Blue" = "blue", "Brown" = "brown", 
+                                "Green" = "green", "Orange" = "orange", "Red" = "red",
+                                "Yellow" = "yellow"))
+
